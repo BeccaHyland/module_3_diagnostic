5a2c0a5a1c54a9d1a2d27ed79b6abefb61f4b22d
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-17 10:43:42 -0600

    adds station model

---
+++ app/models/station.rb
@@ -0,0 +1,15 @@
+class Station
+  attr_reader :name,
+              :address,
+              :fuel_type,
+              :distance,
+              :access_times
+
+  def initialize(data)
+    @name     = data[:name]
+    @address    = data[:address]
+    @fuel_type    = data[:fuel_type]
+    @distance    = data[:distance]
+    @access_times = data[:access_times]
+  end
+end
